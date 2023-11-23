2eeb99f0c407cbc766ea6feef8d1d74e2051c4e2
Kajanan02 <Kajanan02000@gmail.com>
2023-11-23 10:38:53 +0530

    changes

--- backend/modals/calenderModal.js
+++ backend/modals/calenderModal.js
@@ -7,11 +7,11 @@ const calenderSchema = new mongoose.Schema({
         required: true
     },
     start: {
-        type: Date,
+        type: String,
         required: true
     },
     end: {
-        type: Date,
+        type: String,
         required: true
     },
     color: {
